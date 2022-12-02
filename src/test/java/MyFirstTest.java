5afbdebc391589c207dd5e2739dc238f52ed7c8f
Marco Behler <marco@marcobehler.com>
2022-12-02 17:27:52 +0100

    test

---
+++ src/test/java/MyFirstTest.java
@@ -0,0 +1,11 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class MyFirstTest {
+
+    @Test
+    public void first_method() {
+        assertEquals(1,1);
+    }
+}
